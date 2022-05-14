3f9a3fec5d108c12c99f175611046278e1c7c308
dimitris47 <dimitrisinbox@gmail.com>
2022-05-14 11:07:35 +0300

    add activities

--- app/src/main/java/com/dpsoftware/secretsantam/MainActivity.kt
+++ app/src/main/java/com/dpsoftware/secretsantam/MainActivity.kt
@@ -4,14 +4,29 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private var numberOfPlayers: EditText? = null
+    private var enterNames: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        numberOfPlayers = findViewById(R.id.numberOfPlayersEdit)
+        enterNames = findViewById(R.id.enterNamesBtn)
+
+        enterNames!!.setOnClickListener {
+            val text: String = numberOfPlayers!!.text.toString()
+            if (text.isNotEmpty())
+                enterNames(text.toInt())
+        }
     }
 
-    fun enterNames() {
+    private fun enterNames(num: Int) {
         val intent = Intent(this, EntriesActivity::class.java).apply {
 
         }
