12f4c03cd40d0ea67175a9e2df537d2d723201d4
dimitris47 <dimitrisinbox@gmail.com>
2022-05-14 12:57:24 +0300

    add calculate method

--- app/src/main/java/com/dpsoftware/secretsantam/EntriesActivity.kt
+++ app/src/main/java/com/dpsoftware/secretsantam/EntriesActivity.kt
@@ -3,13 +3,40 @@ package com.dpsoftware.secretsantam
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class EntriesActivity : AppCompatActivity() {
+
+    var num: Int? = null
+    var calcCount: Int = 0
+    var players: ArrayList<Int>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_entries)
 
         val intent: Intent = intent
-        val num: Int = intent.getIntExtra("num", 3)
+        num = intent.getIntExtra("num", 3)
+
+        val calc: Button = findViewById(R.id.calcBtn)
+        calc.setOnClickListener {
+            calculate()
+
+        }
+    }
+
+    private fun calculate(): Boolean {
+        if (++calcCount == 65535)
+            return false
+
+        for (i in 0 until num!!)
+            players!!.add(i)
+        players!!.shuffle()
+
+        for (i in 0 until num!!)
+            if (players!![i] == i)
+                calculate()
+
+        return true
     }
 }
