5016140ad98c1015f9f6b8be68d4c0d3f4467bb2
dimitris47 <dimitrisinbox@gmail.com>
2022-05-14 12:08:06 +0300

    set spinner

--- app/src/main/java/com/dpsoftware/secretsantam/MainActivity.kt
+++ app/src/main/java/com/dpsoftware/secretsantam/MainActivity.kt
@@ -4,23 +4,30 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
+import android.widget.*
+import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
 
-    private var numberOfPlayers: EditText? = null
+    private var spinner: Spinner? = null
     private var enterNames: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        numberOfPlayers = findViewById(R.id.numberOfPlayersEdit)
+        spinner = findViewById(R.id.spinner)
+        val spinnerArray: ArrayList<String> = ArrayList(3)
+        for (i in 3..99)
+            spinnerArray.add(i.toString())
+        val arrayAdapter: ArrayAdapter<String> = ArrayAdapter<String>(
+            this, android.R.layout.simple_spinner_item, spinnerArray)
+        spinner!!.adapter = arrayAdapter
+        
         enterNames = findViewById(R.id.enterNamesBtn)
 
         enterNames!!.setOnClickListener {
-            val text: String = numberOfPlayers!!.text.toString()
+            val text: String = spinner!!.toString()
             if (text.isNotEmpty())
                 enterNames(text.toInt())
         }
@@ -30,6 +37,7 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, EntriesActivity::class.java).apply {
 
         }
+        intent.putExtra("num", num)
         startActivity(intent)
     }
 }
